a29d8c8d8ebd59714f65d7dd08eaf24c794ef00a
Bsef19m519 <bsef19m519@pucit.edu.pk>
2022-06-03 15:32:54 +0500

    Resource files are added. code is written in java file but there is some image size issue

--- app/src/main/java/com/example/tajwid/LessonActivity2.java
+++ app/src/main/java/com/example/tajwid/LessonActivity2.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,6 +19,38 @@ public class LessonActivity2 extends AppCompatActivity {
         Intent intent = getIntent();
         String letter=intent.getStringExtra("letter");
         textView.setText(letter);
-
+        if(letter.equals("ض")){
+            imageView.setImageResource(R.drawable.hafia);
+        }
+        else if(letter.equals("ء")||letter.equals("ه")||letter.equals("ع")||letter.equals("ح")||letter.equals("غ")||letter.equals("خ")){
+            imageView.setImageResource(R.drawable.halaqia);
+        }
+        else if(letter.equals("ق")||letter.equals("ك")){
+            imageView.setImageResource(R.drawable.lahatia);
+        }
+        else if(letter.equals("ط")||letter.equals("ذ")||letter.equals("ث")){
+            imageView.setImageResource(R.drawable.laswia);
+        }
+        else if(letter.equals("ا")||letter.equals("و")||letter.equals("ي")){
+            imageView.setImageResource(R.drawable.madda);
+        }
+        else if(letter.equals("ت")||letter.equals("د")||letter.equals("ط")){
+            imageView.setImageResource(R.drawable.natia);
+        }
+        else if(letter.equals("س")||letter.equals("ص")||letter.equals("ز")){
+            imageView.setImageResource(R.drawable.sagaria);
+        }
+        else if(letter.equals("ب")||letter.equals("م")){
+            imageView.setImageResource(R.drawable.shafoia);
+        }
+        else if(letter.equals("ف")){
+            imageView.setImageResource(R.drawable.shafoia2);
+        }
+        else if(letter.equals("ج")||letter.equals("ش")){
+            imageView.setImageResource(R.drawable.shajria);
+        }
+        else if(letter.equals("ل")||letter.equals("ن")||letter.equals("ر")){
+            imageView.setImageResource(R.drawable.zalqia);
+        }
     }
 }
