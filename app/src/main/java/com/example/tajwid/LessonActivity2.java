7216f2cd9d9a09c34fd94fc250fdc549b43736a1
Bsef19m519 <bsef19m519@pucit.edu.pk>
2022-06-05 15:46:05 +0500

    Image resolution issue resolved

--- app/src/main/java/com/example/tajwid/LessonActivity2.java
+++ app/src/main/java/com/example/tajwid/LessonActivity2.java
@@ -4,53 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LessonActivity2 extends AppCompatActivity {
     TextView textView;
     ImageView imageView;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson2);
-        textView=findViewById(R.id.textView2);
-        imageView=findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView2);
+        imageView = findViewById(R.id.imageView);
         Intent intent = getIntent();
-        String letter=intent.getStringExtra("letter");
+        String letter = intent.getStringExtra("letter");
         textView.setText(letter);
-        if(letter.equals("ض")){
+        if (letter.equals("ض")) {
             imageView.setImageResource(R.drawable.hafia);
-        }
-        else if(letter.equals("ء")||letter.equals("ه")||letter.equals("ع")||letter.equals("ح")||letter.equals("غ")||letter.equals("خ")){
+        } else if (letter.equals("ء") || letter.equals("ه") || letter.equals("ع") || letter.equals("ح") || letter.equals("غ") || letter.equals("خ")) {
             imageView.setImageResource(R.drawable.halaqia);
-        }
-        else if(letter.equals("ق")||letter.equals("ك")){
+        } else if (letter.equals("ق") || letter.equals("ك")) {
             imageView.setImageResource(R.drawable.lahatia);
-        }
-        else if(letter.equals("ط")||letter.equals("ذ")||letter.equals("ث")){
+        } else if (letter.equals("ظ") || letter.equals("ذ") || letter.equals("ث")) {
             imageView.setImageResource(R.drawable.laswia);
-        }
-        else if(letter.equals("ا")||letter.equals("و")||letter.equals("ي")){
+        } else if (letter.equals("ا") || letter.equals("و") || letter.equals("ي")) {
             imageView.setImageResource(R.drawable.madda);
-        }
-        else if(letter.equals("ت")||letter.equals("د")||letter.equals("ط")){
+        } else if (letter.equals("ت") || letter.equals("د") || letter.equals("ط")) {
             imageView.setImageResource(R.drawable.natia);
-        }
-        else if(letter.equals("س")||letter.equals("ص")||letter.equals("ز")){
+        } else if (letter.equals("س") || letter.equals("ص") || letter.equals("ز")) {
             imageView.setImageResource(R.drawable.sagaria);
-        }
-        else if(letter.equals("ب")||letter.equals("م")){
+        } else if (letter.equals("ب") || letter.equals("م")) {
             imageView.setImageResource(R.drawable.shafoia);
-        }
-        else if(letter.equals("ف")){
+        } else if (letter.equals("ف")) {
             imageView.setImageResource(R.drawable.shafoia2);
-        }
-        else if(letter.equals("ج")||letter.equals("ش")){
+        } else if (letter.equals("ج") || letter.equals("ش")) {
             imageView.setImageResource(R.drawable.shajria);
-        }
-        else if(letter.equals("ل")||letter.equals("ن")||letter.equals("ر")){
+        } else if (letter.equals("ل") || letter.equals("ن") || letter.equals("ر")) {
             imageView.setImageResource(R.drawable.zalqia);
         }
+        button = findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LessonActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
