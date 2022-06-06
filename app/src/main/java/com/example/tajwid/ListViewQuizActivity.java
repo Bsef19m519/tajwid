3ae32a4d544fd987487db8f4abc6a099967d635b
Bsef19m519 <bsef19m519@pucit.edu.pk>
2022-06-06 21:22:09 +0500

    just one step ahead of version 3.

--- app/src/main/java/com/example/tajwid/ListViewQuizActivity.java
+++ app/src/main/java/com/example/tajwid/ListViewQuizActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 
 import java.util.ArrayList;
 
@@ -27,40 +28,47 @@ public class ListViewQuizActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        button2=findViewById(R.id.button11);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ListViewQuizActivity.this,ResultActivity.class);
-                intent.putExtra("score",score);
-                startActivity(intent);
-            }
-        });
+
         listView=findViewById(R.id.listQuiz);
         //arraylist creation
         ArrayList<MultipleChoiceQuestion> arrayList = new ArrayList<MultipleChoiceQuestion>();
         arrayList.add(new MultipleChoiceQuestion(
             "Q1","حروف حلقی کی نشاندہی کریں۔",
-            "ح","ق","ک","ب",1
+            "ح","ق","ک","ب",R.id.radioA
         ));
         arrayList.add(new MultipleChoiceQuestion(
                 "Q2","مخرج؛ زبان کی نوک جب اوپر اور نیچے کے اگلے دونوں دانتوں کے درمیان آ جاےٴ۔"
-                ,"و","ی","ل","ص",4
+                ,"و","ی","ل","ص",R.id.radioD
         ));
         arrayList.add(new MultipleChoiceQuestion(
                 "Q3","مندرجہ ذیل میں سے کونسا حرف حروف ذلقیہ نہیں ہے۔"
-                ,"ل","ف","ن","ر",2
+                ,"ل","ف","ن","ر",R.id.radioB
         ));
         arrayList.add(new MultipleChoiceQuestion(
                 "Q4","ۛۛۛمخرج؛ دونوں ہونٹوں کی بیرونی تری ملنے سے۔"
-                ,"ن","م","ب","و",2
+                ,"ن","م","ب","و",R.id.radioB
         ));
         arrayList.add(new MultipleChoiceQuestion(
                 "Q5","حروف نطعیہ کی نشاندہی کریں۔"
-                ,"ب","ق","د","ز",3
+                ,"ب","ق","د","ز",R.id.radioC
         ));
 
         MyQuizAdapter adapter = new MyQuizAdapter(this,arrayList);
         listView.setAdapter(adapter);
+        button2=findViewById(R.id.button11);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(int ind=0;ind<arrayList.size();ind++){
+                    int correct = arrayList.get(ind).getCorrectOption();
+                    //View v=adapter.getView(ind,R.layout.activity_quiz_layout,);
+                    RadioGroup radioGroup=findViewById(R.id.rGroup);
+                    if(correct==radioGroup.getCheckedRadioButtonId()) score++;
+                }
+                Intent intent = new Intent(ListViewQuizActivity.this,ResultActivity.class);
+                intent.putExtra("score",score);
+                startActivity(intent);
+            }
+        });
     }
 }
